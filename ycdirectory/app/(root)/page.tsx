603b3a2d393895cf25c87475abe24bdf270b7138
Darshan <darshanpatel2923@gmail.com>
2025-02-22 17:16:26 +0530

    Started with structure and basic structure of app

---
+++ ycdirectory/app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import SearchForm from "../components/SearchForm";
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Noticed in Virtual Competitions.
+        </p>
+        <SearchForm />
+      </section>
+    </>
+  );
+}
---
+++ ycdirectory/app/components/SearchForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Form from 'next/form';
+
+const SearchForm = () => {
+
+    const query = "";
+
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement;
+
+        if (form) {
+            form.reset();
+        }
+    }
+
+    return (
+        <Form action='/' scroll={false} className='search-form'>
+            <input type="text" name='query' defaultValue="" className='search-input' placeholder='Search Startups' />
+            <div className="flex gap-2">
+                {
+                    query && (
+                        <button type='reset' onClick={reset}></button>
+                    )
+                }
+            </div>
+        </Form>
+    )
+}
+
+export default SearchForm
