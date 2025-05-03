39625059dad2642c2ccd6f9a2c5f0cfece826ae2
Darshan <darshanpatel2923@gmail.com>
2025-05-03 21:09:36 +0530

    Setting up our new project based on psychologist appointment booking system in NextJs. Added styles and setup required for our app

---
+++ psychbook/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl underline">Home</h1>
+      <Button>Press Me</Button>
+    </div>
+  )
+}
+
+export default Home
